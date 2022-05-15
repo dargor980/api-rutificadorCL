068bc23e9f2c7640138543571db9f4e61bcb8cbc
Germán Contreras Améstica <german.contrerasa@utem.cl>
2022-05-15 18:01:39 -0400

    FIX: uri paths para produccion

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(require('./controllers/routes/index'));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE', 'OPTIONS', 'HEAD');
